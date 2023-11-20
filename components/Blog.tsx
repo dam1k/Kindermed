697d19b45d0be315b535050649f4d9805cded143
damik <damianbostan12@gmail.com>
2023-11-20 09:37:03 +0200

    responsive stuff

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -3,7 +3,7 @@ import { GraphQLClient, gql } from "graphql-request";
 import BlogSidebar from "./BlogSidebar";
 
 const client = new GraphQLClient(
-  "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clnzv3vau4jnn01t404al79um/master",
+  "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clnzv3vau4jnn01t404al79um/master"
 );
 
 const QUERY = gql`
@@ -45,27 +45,29 @@ async function Blog() {
     <div className="bg-lightGreyBg">
       <div
         id="blog"
-        className="max-[1024px]:hidden max-[1600px]:justify-center max-[1600px]:grid-cols-1 container !pr-0 !pb-[125px] !pt-[125px] grid grid-cols-2 gap-x-[25px] gap-y-[85px]"
+        className="max-[1600px]:justify-center max-[1600px]:grid-cols-1 container max-[1024px]:px-[10px] !pb-[125px] max-[1024px]:!pt-[100px] min-[1025px]:!pt-[125px] grid grid-cols-2 gap-x-[25px] gap-y-[85px] max-[1024px]:!gap-y-[35px] "
       >
         <div>
-          <div className="flex min-[1024px]:gap-[100px] min-[1615px]:gap-[100px] min-[1815px]:gap-[205px]">
-            <p className="uppercase text-black/[0.65] mt-[10px] ml-[3px] leading-[105%]">
+          <div className="flex items-center gap-[50px] min-[1025px]:gap-[100px] min-[1615px]:gap-[100px] min-[1815px]:gap-[205px]">
+            <p className="uppercase text-black/[0.65] min-[1025px]:mt-[10px] min-[1025px]:ml-[3px] text-[12px] min-[1025px]:text-[16pxs] leading-[105%]">
               Blog
             </p>
-            <h2 className="uppercase text-[35px] text-black leading-[140%] tracking-[-0.35px]">
+            <h2 className="uppercase text-[20px] min-[1025px]:text-[35px] text-black leading-[140%] tracking-[-0.35px]">
               Informează-te despre{" "}
-              <span className="text-blue">cele mai noi</span>
+              <span className="max-[1024px]:hidden text-blue">
+                cele mai noi
+              </span>
             </h2>
           </div>
-          <h2 className="uppercase text-[35px] text-black leading-[140%] tracking-[-0.35px]">
+          <h2 className="uppercase text-[20px] min-[1025px]:text-[35px] text-black leading-[140%] tracking-[-0.35px]">
             noutăți și evenimente de la KinderMed
           </h2>
         </div>
-        <div />
+        <div className="max-[1024px]:hidden" />
 
-        <div className="ml-[142px] min-[1815px]:ml-[250px] min-[1615px]:ml-[142px] leading-[145%] flex flex-col gap-[5px]">
-          <span className="text-blue">#citatulzilei</span>
-          <h3 className="w-[500px] text-[22px] text-black italic">
+        <div className="max-[1024px]:mb-[30px] min-[1025px]:ml-[142px] min-[1815px]:ml-[250px] min-[1615px]:ml-[142px] leading-[145%] flex flex-col gap-[5px]">
+          <span className="text-blue max-[1024px]:hidden">#citatulzilei</span>
+          <h3 className="min-[1025px]:w-[500px] text-[14px] min-[1025px]:text-[22px] text-black italic">
             “Fii la curent cu cele mai recente noutăți din domeniul medical și
             cu evenimentele importante organizate de KinderMed.”
           </h3>
--- components/BlogSidebar.tsx
+++ components/BlogSidebar.tsx
@@ -9,7 +9,7 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
   const router = useRouter();
 
   return (
-    <div className="mr-[225px]">
+    <div className="min-[1025px]:mr-[225px]">
       <div className="flex flex-col gap-[25px]">
         <p className="text-[22px] leading-[145%]">Ultimele articole:</p>
         {posts &&
@@ -17,12 +17,12 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
             return (
               <div className="flex gap-[25px]" key={post.id}>
                 <img
-                  className="object-cover rounded-[14px] w-[200px] h-[125px]"
+                  className="object-cover rounded-[14px] max-[1024px]:h-[90px] max-[1024px]:w-[90px] min-[1025px]:w-[200px] min-[1025px]:h-[125px]"
                   src={post.image.url}
                   alt=""
                 />
                 <div className="">
-                  <div className="flex mb-[8px] gap-[15px]">
+                  <div className="max-[1024px]:hidden flex mb-[8px] gap-[15px]">
                     {post.tags.map((tag: string) => {
                       return (
                         <span
@@ -32,10 +32,10 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
                                   tag === "important"
                                     ? "text-pink"
                                     : tag === "info"
-                                    ? "text-yellow"
-                                    : tag === "lifehacks"
-                                    ? "text-purple"
-                                    : ""
+                                      ? "text-yellow"
+                                      : tag === "lifehacks"
+                                        ? "text-purple"
+                                        : ""
                                 }
                                 text-xs leading-[120%]
                                 `}
@@ -45,19 +45,44 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
                       );
                     })}
                   </div>
-                  <h3 className="uppercase text-[20px] leading-[135%] w-[460px] ellipsis">
+                  <h3 className="uppercase text-[16px] min-[1025px]:text-[20px] leading-[135%] w-full ellipsis">
                     {post.title.split(" ").slice(0, 4).join(" ")}
                     <br />
                     {post.title.split(" ").slice(4).join(" ")}
                   </h3>
-                  <p className="mt-[35px] leading-[105%] text-lightGrey">
-                    {formatDate(new Date(post?.publishedAt))}
-                  </p>
+                  <div className="flex max-[1024px]:mt-[4px] justify-between items-center ">
+                      <div className="min-[1025px]:hidden flex mb-[8px] gap-[15px]">
+                          {post.tags.map((tag: string) => {
+                              return (
+                                  <span
+                                      key={tag}
+                                      className={`
+                                ${
+                                          tag === "important"
+                                              ? "text-pink"
+                                              : tag === "info"
+                                                  ? "text-yellow"
+                                                  : tag === "lifehacks"
+                                                      ? "text-purple"
+                                                      : ""
+                                      }
+                                text-xs leading-[120%]
+                                `}
+                                  >
+                            {"#" + tag}
+                          </span>
+                              );
+                          })}
+                      </div>
+                    <p className="max-[1024px]:text-[12px] min-[1025px]:mt-[32px] leading-[105%] text-lightGrey">
+                      {formatDate(new Date(post?.publishedAt))}
+                    </p>
+                  </div>
                 </div>
               </div>
             );
           })}
-        {posts &&
+        {/* {posts &&
           posts.map((post: any) => {
             return (
               <div className="flex gap-[25px]" key={post.id}>
@@ -77,10 +102,10 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
                                   tag === "important"
                                     ? "text-pink"
                                     : tag === "info"
-                                    ? "text-yellow"
-                                    : tag === "lifehacks"
-                                    ? "text-purple"
-                                    : ""
+                                      ? "text-yellow"
+                                      : tag === "lifehacks"
+                                        ? "text-purple"
+                                        : ""
                                 }
                                 text-xs leading-[120%]
                                 `}
@@ -101,8 +126,8 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
                 </div>
               </div>
             );
-          })}
-        {posts &&
+          })} */}
+        {/* {posts &&
           posts.map((post: any) => {
             return (
               <div className="flex gap-[25px]" key={post.id}>
@@ -122,10 +147,10 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
                                   tag === "important"
                                     ? "text-pink"
                                     : tag === "info"
-                                    ? "text-yellow"
-                                    : tag === "lifehacks"
-                                    ? "text-purple"
-                                    : ""
+                                      ? "text-yellow"
+                                      : tag === "lifehacks"
+                                        ? "text-purple"
+                                        : ""
                                 }
                                 text-xs leading-[120%]
                                 `}
@@ -146,7 +171,7 @@ const BlogSidebar = ({ posts }: { posts: any }) => {
                 </div>
               </div>
             );
-          })}
+          })} */}
       </div>
       <Button
         variant="lightGrey"
