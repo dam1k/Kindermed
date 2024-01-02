a8b5a101f91819af848f015e557a0e4284baa28e
damik <damianbostan12@gmail.com>
2024-01-02 03:21:53 +0200

    fix

--- app/vaccinare/page.tsx
+++ app/vaccinare/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Image from "next/image"
 
 function Vaccination() {
     return (
         <div className="overflow-auto min-[1400px]:flex min-[1400px]:items-center min-[1400px]:justify-center !my-[100px] ">
             <div className="shrink-0 h-[743px] w-[1350px]">
-                <Image src="/Vaccination.svg" alt="vaccinare" fill/>
+                <img src="/Vaccination.svg" className=" w-full"/>
             </div>
         </div>
     );
