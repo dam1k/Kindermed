aadcac88b2a674c09cc73a75991204460724ee6b
damik <damianbostan12@gmail.com>
2024-01-29 10:07:24 +0200

    changes vaccination

--- app/vaccinare/page.tsx
+++ app/vaccinare/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 function Vaccination() {
   return (
     <>
-      <div className="flex items-center justify-center uppercase gap-[15px] text-center px-4 min-[1400px]:text-[55px] text-[30px] min-[1400px]:leading-[77px] mt-[75px]">
+      <div className="flex items-center justify-center uppercase gap-[10px] min-[600px]:gap-[15px] text-center px-4 text-[20px] min-[600px]:text-[30px] min-[1400px]:text-[55px] min-[1400px]:leading-[77px] mt-[75px]">
         <h1>În Curând și</h1>
-        <div className="bg-[#00AAF1] px-[27px] text-white rounded-[66px]">
+        <div className="bg-[#00AAF1] px-[10px] min-[600px]:px-[27px] text-white rounded-[66px]">
           la KinderMed!
         </div>
       </div>
